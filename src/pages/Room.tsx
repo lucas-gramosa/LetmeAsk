8006797d4b666fb008a886f12da2fcab93594717
Lucas Matheus Hallier Vieira Gramosa <Lucas.Gramosa@antaq.gov.br>
2022-06-27 16:38:01 -0300

    Fourth Commit

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,32 +1,66 @@
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/Button";
 import '../styles/room.scss';
+import {RoomCode} from '../components/RoomCode';
+import { useParams } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase'
 
+
+type RoomParams = {
+    id:string;
+}
 
 export function Room() {
+    const { user } = useAuth();
+    const params = useParams<RoomParams>();
+    const roomId = params.id;
+    const [newQuestion, setNewQuestion] =useState('');
+
+    async function handleSendQuestion(event: FormEvent) {
+        event.preventDefault();
+        if(newQuestion.trim() === '') {
+            return;
+        }
+        if(!user) {
+           throw new Error('You must be logged in')
+        }
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user?.name,
+                avatar: user.avatar,
+            },
+            isHighLighted: false,
+            isAnswered: false
+        };
+        await database.ref(`rooms/${roomId}/questions`).push(question);
+
+    }
+
     return (
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask"/>
-                    <div>codigo</div>
+                    <RoomCode code={roomId!} />
                 </div>
-            </header>  
-
+            </header>
             <main>
-                <div className="room-title">
+                <div className="room-title"> 
                     <h1>Sala React</h1>
                     <span>4 Perguntas</span>
                 </div>
-            
-                <form>
+                <form onSubmit={handleSendQuestion}>
                     <textarea 
                         placeholder="O que voce quer perguntar?"
+                        onChange={event => setNewQuestion(event.target.value)}
+                        value={newQuestion}
                     />
-
                     <div className="form-footer">
                         <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-                        <Button type="submit">Enviar pergunta</Button>
+                        <Button type="submit" disabled={!user}>Enviar pergunta</Button>
                     </div>
                 </form>
             </main>
